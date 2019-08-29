a3aad18e5b6f813c03ca40c2b3b1cacfa24cf9fb
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-29 18:54:25 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
   def categories_attributes=(categories_attributes)
     categories_attributes.each do |i, category_attributes|
       if category_attributes[:name].present?
-        category =  Category.find_or_create_by(name:  category_attributes[:name])
+        category = Category.find_or_create_by(name:  category_attributes[:name])
         if !self.categories.include?(category)
           self.post_categories.build(:category => category)
         end
